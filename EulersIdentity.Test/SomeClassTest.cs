1a1d8e33be419132709b1375e4c260d347d15a03
Simon Bridewell <simon@sbridewell.me.uk>
2025-08-22 18:12:10 +0100

    Reset solution to initial state

---
+++ EulersIdentity.Test/SomeClassTest.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="SomeClassTest.cs" company="Simon Bridewell">
+// Copyright (c) Simon Bridewell.
+// Released under the MIT license - see LICENSE.txt in the repository root.
+// </copyright>
+
+namespace Sde.EulersIdentity.Test
+{
+    /// <summary>
+    /// Unit tests for <see cref="SomeClass"/>.
+    /// </summary>
+    public class SomeClassTest
+    {
+        /// <summary>
+        /// Tests the SetsSomeProperty method.
+        /// </summary>
+        [Fact]
+        public void DoSomething_SetsSomeProperty()
+        {
+            // Arrange
+            var someClass = new SomeClass();
+            int expectedValue = 42;
+
+            // Act
+            someClass.DoSomething(expectedValue);
+
+            // Assert
+            Assert.Equal(expectedValue, someClass.SomeProperty);
+        }
+    }
+}
---
+++ EulersIdentity/SomeClass.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="SomeClass.cs" company="Simon Bridewell">
+// Copyright (c) Simon Bridewell.
+// Released under the MIT license - see LICENSE.txt in the repository root.
+// </copyright>
+
+namespace Sde.EulersIdentity
+{
+    /// <summary>
+    /// A dummy class so that we have something to test.
+    /// </summary>
+    public class SomeClass
+    {
+        /// <summary>
+        /// Gets a property.
+        /// </summary>
+        public int SomeProperty { get; private set; }
+
+        /// <summary>
+        /// A method which sets the property.
+        /// </summary>
+        /// <param name="someParameter">The parameter to set the property to.</param>
+        public void DoSomething(int someParameter)
+        {
+            this.SomeProperty = someParameter;
+        }
+    }
+}
